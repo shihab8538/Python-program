09e5dd742e3c89202e469b79b1764c83b65bb83c
shihab8538 <shihabkhan7078@gmail.com>
2026-06-13 14:49:20 +0530

    First commit

---
+++ String1.py
@@ -0,0 +1,23 @@
+name ="sibhaxc"
+# include 4 index n
+ns = name[:4]
+print(ns)
+c =name[2:]
+print(c)
+print(name[1:4])
+print(name[-4:-1])
+# name[:4] this means from index 0 to 3
+# name[2:] this means from index 2 to length
+n ="nayra"
+print(len(n))
+print(n.endswith("a"))
+print(n.startswith("a"))
+print(n.capitalize())
+s = "i love my india and india is a very good country"
+co= s.count("i")
+c=s.count("india")
+print(c)
+print(co)
+i =s.find("cou")
+print(i)
+print(s.replace("good","best"))
