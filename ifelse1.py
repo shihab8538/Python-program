ca5b2420b5186c836400193246f40ad17e2d05b3
shihab8538 <shihabkhan7078@gmail.com>
2026-06-13 15:10:40 +0530

    ifelse

---
+++ ifelse1.py
@@ -0,0 +1,11 @@
+a =int(input("enter the age"))
+if(a>18):
+    print("you are eligible")
+elif(a<0):
+     print("Invalid age")
+else:
+     print("you are not  eligible")
+
+# if elif else ladder
+
+print("you are welcome")
